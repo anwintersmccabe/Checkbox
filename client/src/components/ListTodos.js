f4e0c7fab4e9c81bcb211ae19db10914cabea616
Annabel Winters-Mccabe <annabelrosew3@gmail.com>
2021-01-14 12:11:20 -0800

    client code

---
+++ client/src/components/ListTodos.js
@@ -0,0 +1,95 @@
+import React, { Fragment, useEffect, useState } from "react";
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
+
+import EditTodo from "./EditTodos";
+
+const ListTodos = () => {
+  const [todos, setTodos] = useState([]);
+
+  //delete todo function
+
+  const deleteTodo = async id => {
+    try {
+      const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+        method: "DELETE"
+      });
+
+      setTodos(todos.filter(todo => todo.todo_id !== id));
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  const getTodos = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/todos");
+      const jsonData = await response.json();
+
+      setTodos(jsonData);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  console.log(todos);
+
+  return (
+    <Fragment>
+      {" "}
+      <table class="table mt-5 text-center">
+        <thead>
+          <tr>
+            <th>Completed? </th>
+            <th>Description</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/*<tr>
+            <td>John</td>
+            <td>Doe</td>
+            <td>john@example.com</td>
+          </tr> */}
+          {todos.map(todo => (
+            <tr key={todo.todo_id}>
+            <td>
+            <FormControlLabel control={
+            <Checkbox
+            icon={<CheckBoxOutlineBlankIcon fontSize="large" />}
+            checkedIcon={<CheckBoxIcon fontSize="large" />}
+            color = "primary"
+            name="checkedI"
+            />
+          }
+          label=""
+          />
+          </td>
+              <td>{todo.description}</td>
+              <td>
+                <EditTodo todo={todo} />
+              </td>
+              <td>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => deleteTodo(todo.todo_id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Fragment>
+  );
+};
+
+export default ListTodos;
